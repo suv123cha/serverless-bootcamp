f5978f3cd183493990a78fbedd45f42c936daf2e
SUVAJIT <>
2021-12-29 01:35:36 +0530

    Json Schema Validations

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -22,7 +22,7 @@ async function createAuction(event, context) {
             amount: 0,
         },
         createdAT: now.toISOString(),
-        endendAT: endDate.toISOString()
+        endingAt: endDate.toISOString()
     };
 
     try 
--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -1,41 +1,41 @@
 import AWS from 'aws-sdk';
-//middleware
-import middleware from '../lib/middleware';
 import createError from 'http-errors';
+import validator from '@middy/validator';
+import middleware from '../lib/middleware';
+import getAuctionsSchema from '../lib/schemas/getAuctionsSchema';
 
-
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function getAuctions(event, context) {
-    const { status } = event.queryStringParameters;
-    let auctions;
-  
-    const params = {
-        TableName: process.env.AUCTIONS_TABLE_NAME,
-        IndexName: 'statusAndEndDate',
-        KeyConditionExpression: '#status = :status',
-        ExpressionAttributeValues: {
-            ':status': status,
-        },
-        ExpressionAttributeNames: {
-            '#status': 'status',
-        },
-    };
+  const { status } = event.queryStringParameters;
+  let auctions;
 
-    console.log(params);
-    try {
-        const result = await dynamoDB.query(params).promise();
-        console.log(result);
-        auctions = result.Items;
-    } catch (error) {
-        console.log(error);
-        throw new createError.InternalServerError(error);
-    }
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    IndexName: 'statusAndEndDate',
+    KeyConditionExpression: '#status = :status',
+    ExpressionAttributeValues: {
+      ':status': status,
+    },
+    ExpressionAttributeNames: {
+      '#status': 'status',
+    },
+  };
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ response: auctions }),
-    };
+  try {
+    const result = await dynamodb.query(params).promise();
+
+    auctions = result.Items;
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(auctions),
+  };
 }
 
-export const handler = middleware(getAuctions);
+export const handler = middleware(getAuctions)
+  .use(validator({ inputSchema: getAuctionsSchema, useDefaults: true }));
